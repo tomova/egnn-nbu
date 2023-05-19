69d8dffdb281061cf0e01dfddac8ff78e9ba8e7e
Dana Tomova <dana.tomova@student.kit.edu>
2023-05-20 02:13:05 +0300

    EquivariantGNN

--- EGNN.py
+++ EGNN.py
@@ -4,6 +4,9 @@ from torch.nn import functional as F
 from torch_geometric.nn import global_mean_pool
 from se3_transformer_pytorch import SE3Transformer
 
+# Define the mapping from atomic numbers to indices
+atomic_number_to_index = {1: 0, 6: 1, 7: 2, 8: 3, 9: 4}
+
 class EquivariantGNN(torch.nn.Module):
     def __init__(self, num_node_features, hidden_dim, output_dim, num_node_types):
         super(EquivariantGNN, self).__init__()
@@ -11,7 +14,7 @@ class EquivariantGNN(torch.nn.Module):
         self.embedding = torch.nn.Embedding(num_embeddings=num_node_types, embedding_dim=num_node_features)
 
         self.transformer = SE3Transformer(
-            dim=8,  
+            dim=num_node_features,  
             heads=8,
             dim_head=16, 
             num_degrees=4
@@ -20,7 +23,10 @@ class EquivariantGNN(torch.nn.Module):
         self.lin = Lin(hidden_dim, output_dim)
 
     def forward(self, data):
-        x = torch.cat([data.pos, self.embedding(data.z)], dim=-1)  # Combine position and atom type information
+        # Map atomic numbers to indices
+        z_indices = data.z.apply_(lambda x: atomic_number_to_index[x.item()])  
+
+        x = torch.cat([data.pos, self.embedding(z_indices)], dim=-1)  # Combine position and atom type information
         x = x.unsqueeze(0)
         edges = data.edge_index.unsqueeze(0)
         edge_attr = data.edge_attr.unsqueeze(0)
