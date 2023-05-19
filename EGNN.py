7e9c32f32a2dd077e72e258172cefcb71f60c9bd
Dana Tomova <dana.tomova@student.kit.edu>
2023-05-19 22:44:01 +0300

    fix transformer

--- EGNN.py
+++ EGNN.py
@@ -8,16 +8,29 @@ class EquivariantGNN(torch.nn.Module):
     def __init__(self, num_node_features, hidden_dim, output_dim, num_node_types):
         super(EquivariantGNN, self).__init__()
         self.embedding = torch.nn.Embedding(num_embeddings=num_node_types, embedding_dim=num_node_features)
-        self.transformer1 = SE3Transformer(num_node_features, hidden_dim)
-        self.transformer2 = SE3Transformer(hidden_dim, hidden_dim)
+
+        transformer_config = {
+            'num_layers': 4,
+            'num_channels': num_node_features,
+            'num_degrees': 4,
+            'div': 4,
+            'dim_input': num_node_features,
+            'dim_output': hidden_dim,
+            'num_heads': 1,
+        }
+
+        self.transformer1 = SE3Transformer(transformer_config)
+        transformer_config['dim_input'] = hidden_dim
+        self.transformer2 = SE3Transformer(transformer_config)
+
         self.lin = Lin(hidden_dim, output_dim)
 
     def forward(self, data):
         x = torch.cat([data.pos, self.embedding(data.z)], dim=-1)  # use embedding for atomic numbers
-        x = self.transformer1(x)
-        x = x.relu()
-        x = self.transformer2(x)
-        x = x.relu()
+        x = self.transformer1(x, data.edge_index)
+        x = F.relu(x)
+        x = self.transformer2(x, data.edge_index)
+        x = F.relu(x)
         x = self.lin(x)
 
         # Use mean for readout
