5456d8063c19e516b94d39903a8d9c6a5a3e6c9a
Dana Tomova <dana.tomova@student.kit.edu>
2023-06-23 15:48:35 +0300

    fix wrong initialization of Gate from e3nn.nn

--- modelE3gnnDipole.py
+++ modelE3gnnDipole.py
@@ -19,7 +19,8 @@ class E3nnModel(torch.nn.Module):
 
         self.tp = FullyConnectedTensorProduct(self.irreps_in, self.irreps_in, self.irreps_hidden)
         self.fc = torch.nn.Linear(self.irreps_hidden.dim, self.irreps_out.dim)
-        self.gate = Gate(self.irreps_out.dim, [self.irreps_out.dim])
+        self.gate = Gate(self.irreps_hidden, self.irreps_hidden, self.irreps_out)
+
 
     def forward(self, data):
         x = torch.cat([data.z.unsqueeze(-1).float(), data.pos], dim=-1)
