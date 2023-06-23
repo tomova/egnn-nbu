50c54916b4be11090fa379efa6c523c97f674c32
Dana Tomova <dana.tomova@student.kit.edu>
2023-06-23 15:45:27 +0300

    improve e3nn dipoles

--- modelE3gnnDipole.py
+++ modelE3gnnDipole.py
@@ -4,6 +4,7 @@ from torch_geometric.nn import global_mean_pool
 from e3nn.nn import FullyConnectedNet
 from e3nn.o3 import FullyConnectedTensorProduct
 from e3nn.o3 import Irreps
+from e3nn.nn import Gate
 from QM93D_MM import QM93D
 import torch.nn.functional as F
 
@@ -12,31 +13,29 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 class E3nnModel(torch.nn.Module):
     def __init__(self):
         super(E3nnModel, self).__init__()
+        self.irreps_in = Irreps("1x0e + 1x1o")
+        self.irreps_hidden = Irreps("2x0e + 2x1o + 1x2e")
+        self.irreps_out = Irreps("1x0e")
 
-        self.fc_pos = FullyConnectedNet([3, 2])
-        self.fc_z = FullyConnectedNet([1, 2])
-        self.fc_out = FullyConnectedNet([2, 3])
+        self.tp = FullyConnectedTensorProduct(self.irreps_in, self.irreps_in, self.irreps_hidden)
+        self.fc = torch.nn.Linear(self.irreps_hidden.dim, self.irreps_out.dim)
+        self.gate = Gate(self.irreps_out.dim, [self.irreps_out.dim])
 
     def forward(self, data):
-        pos = F.relu(self.fc_pos(data.pos))
-        z = F.relu(self.fc_z(data.z.unsqueeze(-1).float()))
-        x = pos * z  # Element-wise multiplication
-        x = self.fc_out(x)
+        x = torch.cat([data.z.unsqueeze(-1).float(), data.pos], dim=-1)
+        x = self.tp(x, x)
+        x = F.relu(x)
+        x = self.fc(x)
+        x = self.gate(x)
         x = global_mean_pool(x, data.batch)  # Pooling operation on a per-graph basis
         return x
-    
+
+# Continue from your existing code...
 # Load data
 dataset = QM93D(root='data')
 
 for data in dataset:
     data.dipole = data.dipole.view(1, 3)
-
-
-for i in range(10):
-    data = dataset[i]
-    print(f'Data point {i}:', data)
-    print('Dipole:', data.dipole, 'Shape:', data.dipole.shape)
-
 
 # Split data into train, validation and test sets
 split_idx = dataset.get_idx_split(len(dataset), train_size=110000, valid_size=10000, seed=42)
@@ -47,18 +46,21 @@ test_dataset = dataset[split_idx['test']]
 # Define the E(3) equivariant GNN model
 Rs_in = [(1, 0, 1)]
 Rs_out = [(1, 0, 1)]
+# Define the E(3) equivariant GNN model
 model = E3nnModel().to(device)
 
 # Define the optimizer and loss function
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
-loss_func = torch.nn.L1Loss()
+scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5)
+loss_funcMSE = torch.nn.MSELoss()
+loss_funcL1 = torch.nn.L1Loss()
 
 # Define the data loaders for each set
 train_loader = DataLoader(train_dataset, batch_size=564, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
 test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-# Define the evaluation function for both MAE and R2
+# Define the evaluation function for both MAE, MSE and R2
 def evaluate(model, data_loader):
     model.eval()
     total_abs_error = 0
@@ -76,13 +78,16 @@ def evaluate(model, data_loader):
             total_variance += torch.var(target, unbiased=False).item() * (target.shape[0] - 1)  # Use reshaped data.dipole
             total_examples += target.shape[0]
     mae = total_abs_error / total_examples
+    mse = total_sq_error / total_examples
     r2 = 1 - (total_sq_error / total_variance)
-    return mae, r2
+    return mae, mse, r2
+
 
 
 # Training loop
 n_epochs = 1000
 validation_interval = 20
+best_val_loss = float('inf')
 
 for epoch in range(n_epochs):
     model.train()
@@ -90,24 +95,30 @@ for epoch in range(n_epochs):
         batch = batch.to(device)
         optimizer.zero_grad()
         out = model(batch)
-        print("Output shape:", out.shape)
-        print("Dipole shape:", batch.dipole.shape)
         target = batch.dipole.view(-1, 3)
-        print("target shape:", target.shape)
-        loss = loss_func(out, target)
+        loss = loss_funcMSE(out, target)
         loss.backward()
         optimizer.step()
 
     if epoch % validation_interval == 0:
-        train_mae, train_r2 = evaluate(model, train_loader)
-        val_mae, val_r2 = evaluate(model, val_loader)
-        print(f"Epoch: {epoch}, Train MAE: {train_mae}, Train R2: {train_r2}, Val MAE: {val_mae}, Val R2: {val_r2}")
+        val_mae, val_mse, val_r2 = evaluate(model, val_loader)
+        print(f"Epoch: {epoch}, Val MAE: {val_mae}, Val MSE: {val_mse}, Val R2: {val_r2}, Best Val Loss: {best_val_loss}")
+        val_loss = val_mse # Use MSE for validation loss
+        scheduler.step(val_loss)
+        if val_loss < best_val_loss:
+            best_val_loss = val_loss
+            torch.save(model.state_dict(), "best_modelE3D.pt")
+
+# Load the best model
+model.load_state_dict(torch.load("best_modelE3D.pt"))
+#-------------------
+    
 
 # Evaluation
 model.eval()
-train_mae, train_r2 = evaluate(model, train_loader)
-val_mae, val_r2 = evaluate(model, val_loader)
-test_mae, test_r2 = evaluate(model, test_loader)
-print(f"Train MAE: {train_mae}, Train R2: {train_r2}")
-print(f"Validation MAE: {val_mae}, Validation R2: {val_r2}")
-print(f"Test MAE: {test_mae}, Test R2: {test_r2}")
+train_mae, train_mse, train_r2 = evaluate(model, train_loader)
+val_mae, val_mse, val_r2 = evaluate(model, val_loader)
+test_mae, test_mse, test_r2 = evaluate(model, test_loader)
+print(f"Train MAE: {train_mae}, Train MSE: {train_mse}, Train R2: {train_r2}")
+print(f"Validation MAE: {val_mae}, Validation MSE: {val_mse}, Validation R2: {val_r2}")
+print(f"Test MAE: {test_mae}, Test MSE: {test_mse}, Test R2: {test_r2}")
